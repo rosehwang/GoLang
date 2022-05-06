1f3e57b0a4dbac0626669cee30639c9856d7a0db
roseHwang <beget12@naver.com>
2022-05-06 18:24:24 +0900

    gochannel 기본 개념 정리

---
+++ src/section09/gochannel.go
@@ -0,0 +1,35 @@
+package main //채널(Channel) 기초(1)
+
+import (
+	"fmt"
+	"time"
+)
+
+func work1(v chan int) {
+	fmt.Println("work1 : S ---> ", time.Now())
+	time.Sleep(1 * time.Second)
+	fmt.Println("work1 : E ---> ", time.Now())
+	v <- 1 //데이터 송신
+}
+
+func work2(v chan int) {
+	fmt.Println("work2 : S ---> ", time.Now())
+	time.Sleep(1 * time.Second)
+	fmt.Println("work2 : E ---> ", time.Now())
+	v <- 2 //데이터 송신
+}
+
+func main() {
+	//채널(Channel)
+	//고루틴간의 상호 정보(데이터) 교환 및 실행 흐름 동기화 위해 사용
+	// 실행 흐름 제어 가능(동기 , 비동기) => 일반 변수로 선언 후 사용 가능
+	// 데이터 전달 자료형 선언 후 사용(지정 된 타입만 주고 받을 수 있음)
+	//interface() 전달을 통해서 자료형 상관없이 전송 및 수신가능.
+	// 값을 전달(복사 후 : bool, int 등), 포인터(슬라이스, 맵) 등을 전달시에는 주의!, 동기화 (Mutex)
+	//멀티프로세싱 처리에서 교착상태(경쟁) 주의!
+	// <- , -> (채널 <- 데이터 : 송신. 데이터를 채널에 보내겠다)
+	// (<- 채널 : 수신. )
+
+	//예제1
+
+}
